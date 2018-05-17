287487def3124fadc6edb46ffab0b323082278f4
Diego García <diego@lateralview.net>
2018-05-17 01:37:27 -0300

    Frontend

---
+++ web/js/firebase.js
@@ -0,0 +1,29 @@
+var pageSize = 5;
+
+var config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  listenToFirebase();
+});
+
+function listenToFirebase() {
+  database.ref('/tweets')
+  .orderByChild('timestamp_ms')
+  .limitToLast(pageSize)
+  .on('child_added', function(snapshot) {
+    // Dispatch 'newteet' event
+    var eventData = { detail: { id: snapshot.key, tweet: snapshot.val() } }
+    var tweetEvent = new CustomEvent('newtweet', eventData)
+    document.dispatchEvent(tweetEvent);
+  });
+}
